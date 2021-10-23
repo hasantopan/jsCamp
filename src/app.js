3d920bff1cedcca4fc332c9187fbe514dedbc16b
hasantopan <hasan_topan@hotmail.com>
2021-10-23 06:56:59 +0300

    REST & SPREAD

--- src/app.js
+++ src/app.js
@@ -34,3 +34,86 @@ function save4 (puan=10, ogrenci) {
     console.log(ogrenci.name + ": " + puan + "  save4 çalıştı")    
 }
 save4(undefined , student)  // UNDEFINED :  ben aslımda yoğum
+
+
+let students = ["Hasan Topan","Duygu Sezgin","Engin"]// array oluşturma, yeni bir referans alınmış olur
+console.log(students)
+
+let students2 = [student, {id:2, name:"Duygu Sezgin"}]
+console.log(students2)
+
+let students3 = [student, {id:2, name:"Duygu Sezgin"}, "Ankara", {city:"Uşak"}]
+console.log(students3)
+
+console.log("\n\n")
+console.log("\n\n")
+
+console.log("*** REST ***")
+
+
+//////////////////////
+// ******  REST ****//
+//////////////////////
+//rest: geriye kalan parametrelerdir , C# daki karşılığı "params"  ,  Java daki karşılığı "varArgs"
+
+let showProducts = function name(id,...products) { // ...products bir arraydir
+    console.log(id)
+    console.log(products)
+}
+
+console.log(typeof showProducts)
+
+console.log("showProducts() çalışacak şimdi...")
+showProducts()
+console.log("showProducts() çalıştı")
+
+console.log("\n\n")
+
+console.log("showProducts(10,'Elma","Armut","Karpuz') çalışacak şimdi...")
+showProducts(10,"Elma","Armut","Karpuz")
+console.log("showProducts(10,'Elma","Armut","Karpuz') çalıştı")
+
+// ** showProducts fonksiyonunda "products[0]" ile sadece Elmaya ulaşabilirisn
+
+console.log("\n\n")
+console.log("\n\n")
+
+console.log("*** SPREAD ***")
+
+//////////////////////
+// ******  SPREAD ****//
+//////////////////////
+//... spraed : ayrıştırmak. "rest"in tersi olarak düşünülebiilir
+
+console.log("console.log(Math.max(1,2,5,60,14,25,-5,9)) çalışacak şimdi...")
+console.log(Math.max(1,2,5,60,14,25,-5,9))
+console.log("console.log(Math.max(1,2,5,60,14,25,-5,9)) çalıştı")
+
+console.log("\n\n")
+
+
+let puanlar = [1,2,5,60,14,25,-5,9]
+
+console.log("console.log(...puanlar)  ve  console.log(Math.max(...puanlar)) çalışacak şimdi...")
+console.log(...puanlar)// dizi olarak değil de ayrı ayrı olarak basar, spread yapmış olur
+console.log(Math.max(...puanlar))
+console.log("console.log(...puanlar)  ve  console.log(Math.max(...puanlar)) çalıştı")
+
+console.log("\n\n")
+
+console.log("console.log('ABC','D','EFG') , console.log(...'ABC','D',...'EFG')---- çalışacak şimdi...")
+console.log("ABC","D","EFG")
+console.log(..."ABC","D",..."EFG")
+console.log("console.log('ABC','D','EFG') , console.log(...'ABC','D',...'EFG')---- çalıştı")
+
+
+
+console.log("\n\n")
+console.log("\n\n")
+
+console.log("*** DESTRUCTURING ***")
+
+
+//////////////////////
+// ******  DESTRUCTURING ****//
+//////////////////////
